08a4138255aa02eb06e46a4b4958dec000c724dd
Adam Storr <WestDiscGolf@users.noreply.github.com>
2023-01-09 22:01:19 +0000

    Add automapper DI extensions nuget

--- AndAutomapper/src/FunctionApp/Program.cs
+++ AndAutomapper/src/FunctionApp/Program.cs
@@ -1,7 +1,17 @@
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices((context, services) =>
+    {
+        services.AddAutoMapper(Assembly.GetAssembly(typeof(Program)));
+    })
     .Build();
 
 host.Run();
+
+public partial class Program
+{
+}
