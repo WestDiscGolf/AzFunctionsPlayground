05b6dfd9a98dc75143985a96bb74fefbfb803cf8
Adam Storr <WestDiscGolf@users.noreply.github.com>
2023-01-11 21:00:14 +0000

    Add settings type and bind to configuration

--- AndConfiguration/src/FunctionApp/Function1.cs
+++ AndConfiguration/src/FunctionApp/Function1.cs
@@ -2,15 +2,18 @@ using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace FunctionApp
 {
     public class Function1
     {
+        private readonly IOptions<Settings> _options;
         private readonly ILogger _logger;
 
-        public Function1(ILoggerFactory loggerFactory)
+        public Function1(IOptions<Settings> options, ILoggerFactory loggerFactory)
         {
+            _options = options;
             _logger = loggerFactory.CreateLogger<Function1>();
         }
 
@@ -22,7 +25,7 @@ namespace FunctionApp
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            response.WriteString(_options.Value.Name);
 
             return response;
         }
--- AndConfiguration/src/FunctionApp/Program.cs
+++ AndConfiguration/src/FunctionApp/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices((host, services) =>
+    {
+        services.Configure<Settings>(host.Configuration.GetSection("Settings"));
+    })
     .Build();
 
 host.Run();
